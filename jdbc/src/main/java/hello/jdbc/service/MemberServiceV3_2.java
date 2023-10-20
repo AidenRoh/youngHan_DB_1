62be80eb4258310b1cc238372a435654463b6e70
AidenRoh <colohrby@gmail.com>
2023-10-20 15:33:00 +0900

    231020

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -1,46 +1,44 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV2;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 메니저
+ * 트랜잭션 - 트랜잭션 템플릿
  */
 
 @Slf4j
-@RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_2 {
 
-
-    //    private final DataSource dataSource;
-    private final PlatformTransactionManager transactionManager;
+//    private final DataSource dataSource;
+//    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        //트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
-        try {
+        txTemplate.executeWithoutResult((status) -> {
             //비즈니스 로직
-            bizLogic( fromId, toId, money);
-            transactionManager.commit(status); //성공시 커밋
-
-        } catch (Exception e) {
-            transactionManager.rollback(status); //실패시 롤백
-            throw new IllegalStateException(e);
-        }
-
-
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -1,46 +1,27 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV2;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 메니저
+ * 트랜잭션 - @Transcational AOP
  */
 
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_3 {
 
-
-    //    private final DataSource dataSource;
-    private final PlatformTransactionManager transactionManager;
     private final MemberRepositoryV3 memberRepository;
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        //트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-
-        try {
-            //비즈니스 로직
-            bizLogic( fromId, toId, money);
-            transactionManager.commit(status); //성공시 커밋
-
-        } catch (Exception e) {
-            transactionManager.rollback(status); //실패시 롤백
-            throw new IllegalStateException(e);
-        }
-
-
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
